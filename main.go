44edcfaf446e889901d3593b550d5ce11f592c1f
Mihaita Ivli <mihaita.ivli@skybettingandgaming.com>
2021-06-11 14:35:37 +0100

    ad simple page with minimal router

--- main.go
+++ main.go
@@ -7,10 +7,18 @@ import (
 
 func main() {
 	http.HandleFunc("/", rootHandle)
-	http.ListenAndServe(":3000", nil)
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		panic("Something's wrong, exiting...")
+	}
 }
 
 func rootHandle(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my wonderful site</h1>")
+	path := r.URL.Path
+	if path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to my wonderful site</h1>")
+	} else if path == "/contact"{
+		fmt.Fprint(w, "<p>To contact us please send an email to <a href=\"mailto:support@localhost.com\">support@localhost.com</a></p>")
+	}
 }
