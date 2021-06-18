f693418076332dfb1b9d8fc6bde92f79ed1f7cdb
Mihaita Ivli <mihaita.ivli@skybettingandgaming.com>
2021-06-18 14:20:57 +0100

    add Gorilla/mux

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/mihaitaivli/goPictures
 go 1.15
 
 require (
+	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/howeyc/fsnotify v0.9.0 // indirect
 	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/pilu/config v0.0.0-20131214182432-3eb99e6c0b9a // indirect
--- main.go
+++ main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
 func main() {
-	mux := &http.ServeMux{}
-	mux.HandleFunc("/", rootHandle)
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
 
-	err := http.ListenAndServe(":3000", mux)
+
+	err := http.ListenAndServe(":3000", r)
 	if err != nil {
 		panic("Something's wrong, exiting...")
 	}
@@ -26,4 +29,14 @@ func rootHandle(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprint(w, "<h3>Not found</h3>")
 	}
+}
+
+func home (w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Welcome to my wonderful site</h1>")
+}
+
+func contact (w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<p>To contact us please send an email to <a href=\"mailto:support@localhost.com\">support@localhost.com</a></p>")
 }
